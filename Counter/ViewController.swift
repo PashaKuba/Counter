db01962c9ba58b7b0e145fc2e08886224c9778fd
Kuba <p.cuba@yandex.ru>
2024-02-29 15:37:53 +0200

    Создан проект

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var historyView: UITextView!
+    
+    var counter = 0 {
+            didSet {
+                counterLabel.text = "\(counter)"
+            }
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        historyView.text = "История изменений:"
+        historyView.isEditable = false
     }
 
+    func appendToHistory(_ entry: String) {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+            let timestamp = dateFormatter.string(from: Date())
+            
+            historyView.text += "\n[\(timestamp)]: \(entry)"
+            let range = NSMakeRange(historyView.text.count - 1, 1)
+            historyView.scrollRangeToVisible(range)
+        }
+    
+    @IBAction func incrementButton(_ sender: UIButton) {
+        counter += 1
+        appendToHistory("значение изменено на +1")
+    }
+    
+    @IBAction func decrementButton(_ sender: UIButton) {
+        if counter > 0 {
+                    counter -= 1
+                    appendToHistory("значение изменено на -1")
+                } else {
+                    appendToHistory("попытка уменьшить значение счётчика ниже 0")
+                }
+    }
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        counter = 0
+                appendToHistory("значение сброшено")
+    }
+    
 
 }
 
